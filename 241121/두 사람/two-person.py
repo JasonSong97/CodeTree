fc6aad16c0002a5ae0242b8c5ef3df0f3c16d159
JasonSong97 <jaegeunsong97@gmail.com>
2024-11-21 00:07:54 +0900

    Title: 두 사람, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

--- 241121/두 사람/two-person.py
+++ 241121/두 사람/two-person.py
@@ -1,6 +1,13 @@
-n = int(input())
+# 첫 번째 사람의 정보 입력받기
+age1, gender1 = input().split()
+age1 = int(age1)
 
-if (n % 2 != 0 and n % 3 == 0) or (n % 2 == 0 and n % 5 == 0):
-    print('true')
+# 두 번째 사람의 정보 입력받기
+age2, gender2 = input().split()
+age2 = int(age2)
+
+# 조건 확인: 한 사람이라도 19세 이상이면서 남자인 경우
+if (age1 >= 19 and gender1 == 'M') or (age2 >= 19 and gender2 == 'M'):
+    print(1)
 else:
-    print('false')
+    print(0)
