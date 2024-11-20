a76498edb67501265624d817e6efefc646819a65
JasonSong97 <jaegeunsong97@gmail.com>
2024-11-20 23:14:21 +0900

    Title: 살 수 있는 물건 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241120/살 수 있는 물건 2/things-able-to-buy-2.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+data = [("book", 3000), ("mask", 1000), ("pen", 500)]
+
+for d in data:
+    if n < 500:
+        print("no")
+    if (n // d[1]) > 1:
+        print(d[0])
+        break
