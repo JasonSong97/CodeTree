ae635ef2d4158adf7c0c7881af0dbccde40c6faf
JasonSong97 <jaegeunsong97@gmail.com>
2024-11-20 23:15:04 +0900

    Title: 살 수 있는 물건 2, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241120/살 수 있는 물건 2/things-able-to-buy-2.py
+++ 241120/살 수 있는 물건 2/things-able-to-buy-2.py
@@ -5,6 +5,7 @@ data = [("book", 3000), ("mask", 1000), ("pen", 500)]
 for d in data:
     if n < 500:
         print("no")
-    if (n // d[1]) > 1:
+        break
+    if (n // d[1]) >= 1:
         print(d[0])
         break
